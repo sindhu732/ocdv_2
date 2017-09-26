a5d5c23d545b3fad1495f60ee7fc6ba2bb95a1fa
Sindhu <sindhu732@gmail.com>
2017-09-26 01:23:32 -0400

    init mean stack

---
+++ client/components/directives.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CollapseModule } from 'ng2-bootstrap';
+
+
+import { RouterModule } from '@angular/router';
+
+import { AuthModule } from './auth/auth.module';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { OauthButtonsComponent } from './oauth-buttons/oauth-buttons.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CollapseModule,
+    AuthModule,
+
+    RouterModule,
+  ],
+  declarations: [
+    NavbarComponent,
+    FooterComponent,
+    OauthButtonsComponent,
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+    OauthButtonsComponent,
+  ]
+})
+export class DirectivesModule {}
